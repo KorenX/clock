1172b96b10e6c457459308ed0997eaedd51718f1
Koren Sanger <korenxking@gmail.com>
2017-11-01 21:37:49 +0200

    Update time

--- clock.py
+++ clock.py
@@ -1,12 +1,16 @@
 import datetime
+import time
 from os import system
 
-s = "# The real time clock!\n\nEver wanted to know what the time is? Well, your journey is about to end!\n\n**The time is "
-time = datetime.datetime.now().strftime('%b %d, %Y, %H:%M') + "**"
 
-with open("readme.md", "w") as f:
-	f.write(s + time)
-	
-system("git add .")
-system("git commit -m \"Update time\"")
-system("git push origin master")
+while True:
+	s = "# The real time clock!\n\nEver wanted to know what the time is? Well, your journey is about to end!\n\n**The time is "
+	t = datetime.datetime.now().strftime('%b %d, %Y, %H:%M') + "**"
+
+	with open("readme.md", "w") as f:
+		f.write(s + t)
+		
+	system("git add .")
+	system("git commit -m \"Update time\"")
+	system("git push origin master")
+	time.sleep(600)
